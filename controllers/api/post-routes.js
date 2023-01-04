febe760bf0f25d992c0e2c379e98adec899787e5
escowin <edwin@escowinart.com>
2023-01-04 13:11:46 -0600

    controllers | post | rest api

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,3 +1,106 @@
 const router = require("express").Router();
+const { Post, User } = require("../../models");
+
+// get | posts | /api/posts
+router.get("/", (req, res) => {
+  console.log("=== get all posts ====");
+  Post.findAll({
+    attributes: ["id", "content", "title", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// restful api | specific post | /api/posts/:id
+// - create
+router.post("/", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    content: req.body.content,
+    user_id: req.body.user_id,
+  })
+    .then((dbPostData) => res.json(dbPostData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// - read
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "post does not exist" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// - update
+router.put("/:id", (req, res) => {
+  Post.update(
+    {
+      title: req.body.title,
+      content: req.body.content,
+    },
+    {
+      where: { id: req.params.id },
+    }
+  )
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        req.status(404).json({ message: "post does not exist" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// - delete
+router.delete("/:id", (req, res) => {
+  Post.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "post does not exist" });
+        return;
+      }
+      res.json(dbPostData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on database and server connection
-// - force true | DROP DATABASE IF EXISTS tech_blog_db;
+// - force: true | DROP DATABASE IF EXISTS tech_blog_db;
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
