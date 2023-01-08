e7edfab1d9d43ad9cd5b88a6db69ec12b6be961e
escowin <edwin@escowinart.com>
2023-01-07 21:42:53 -0600

    dashboard | functional frontend edit & delete js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
-const withAuth = require('../utils/auth');
+const withAuth = require("../utils/auth");
 
 // get | dashboard | /dashboard/ | accessible only to session user
 router.get("/", withAuth, (req, res) => {
@@ -27,6 +27,44 @@ router.get("/", withAuth, (req, res) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
       res.render("dashboard", {
         posts,
+        loggedIn: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "post not found" });
+        return;
+      }
+      // serializes data
+      const post = dbPostData.get({ plain: true });
+
+      res.render("edit-post", {
+        post,
         loggedIn: true,
       });
     })
