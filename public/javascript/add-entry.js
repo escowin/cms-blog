7a65e95b5d2075c8db46bf197e140ba902882121
escowin <edwin@escowinart.com>
2023-03-22 17:16:17 -0500

    captured entry form values

--- public/javascript/add-entry.js
+++ public/javascript/add-entry.js
@@ -1,37 +1,39 @@
 // logic
 async function entryFormHandler(e) {
-  // e.preventDefault();
+  e.preventDefault();
 
-  const date = document.getElementById('entry-date').value.trim();
-
-  console.log(date)
-  const entry_text = document
-    .querySelector('textarea[name="entry-text"]')
-    .value.trim();
-
+  const date = document.getElementById("date").value.trim();
+  const weight = document.getElementById("weight").value.trim();
+  const entry_text = document.getElementById("text").value.trim();
   const journal_id = window.location.toString().split("/")[
     window.location.toString().split("/").length - 1
-  ];
+  ].split("?")[0]
 
-  if (entry_text) {
-    const response = await fetch("/api/entries", {
-      method: "post",
-      body: JSON.stringify({
-        journal_id,
-        entry_text,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
+  console.log(date);
+  console.log(weight);
+  console.log(entry_text);
+  console.log(typeof journal_id);
 
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
-  }
+  // if (entry_text) {
+  //   const response = await fetch("/api/entries", {
+  //     method: "post",
+  //     body: JSON.stringify({
+  //       journal_id,
+  //       entry_text,
+  //     }),
+  //     headers: { "Content-Type": "application/json" },
+  //   });
+
+  //   if (response.ok) {
+  //     document.location.reload();
+  //     console.log("ok")
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
 }
 
 // calls
 document
   .getElementById("entry-form")
-  .addEventListener("click", entryFormHandler);
+  .addEventListener("submit", entryFormHandler);
