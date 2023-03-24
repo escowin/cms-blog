5bac47bd72d994d597fe65a232ebbfc58b24eab4
escowin <edwin@escowinart.com>
2023-03-24 01:34:01 -0500

    set up Tag get & post methods

--- controllers/api/tag-routes.js
+++ controllers/api/tag-routes.js
@@ -1,6 +1,28 @@
-const router = require('express').Router();
-const { Tag, Entry, EntryTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Entry, EntryTag } = require("../../models");
 
 // rest api
+router.post("/", ({ body }, res) => {
+  Tag.create(body)
+    .then(tag => res.status(200).json(tag))
+    .catch(err => res.status(404).json(err));
+});
+
+router.get("/", (req, res) => {
+    Tag.fineAll({
+        include: [
+            {
+                model: Entry,
+                through: EntryTag
+            }
+        ]
+    })
+    .then(tags => res.status(200).json(tags))
+    .catch(err => res.status(404).json(err));
+});
+
+router.get("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
