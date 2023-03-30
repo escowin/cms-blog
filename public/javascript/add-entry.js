3b87f3e8a3f5972982fb384f6adcf199bed36352
escowin <edwin@escowinart.com>
2023-03-30 14:11:20 -0500

    tag-name | logged frontend js input in console

--- public/javascript/add-entry.js
+++ public/javascript/add-entry.js
@@ -2,6 +2,7 @@
 async function entryFormHandler(e) {
   e.preventDefault();
 
+  // entry data
   const entry_date = document.getElementById("date").value.trim().replace(/-/g, ".");
   const entry_weight = document.getElementById("weight").value.trim();
   const entry_text = document.getElementById("text").value.trim();
@@ -9,25 +10,28 @@ async function entryFormHandler(e) {
     window.location.toString().split("/").length - 1
   ].split("?")[0]
 
-  if (entry_text) {
-    const response = await fetch("/api/entries", {
-      method: "post",
-      body: JSON.stringify({
-        journal_id,
-        entry_date,
-        entry_weight,
-        entry_text,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
+  // tag data
+  const tag_name = document.getElementById("tag-name").value.trim();
+  console.log(tag_name)
+  // if (entry_text) {
+  //   const response = await fetch("/api/entries", {
+  //     method: "post",
+  //     body: JSON.stringify({
+  //       journal_id,
+  //       entry_date,
+  //       entry_weight,
+  //       entry_text,
+  //     }),
+  //     headers: { "Content-Type": "application/json" },
+  //   });
 
-    if (response.ok) {
-      document.location.reload();
-      console.log("ok")
-    } else {
-      alert(response.statusText);
-    }
-  }
+  //   if (response.ok) {
+  //     document.location.reload();
+  //     console.log("ok")
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
 }
 
 // calls
