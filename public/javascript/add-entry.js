9e9273ad4ec75002429d9b89ae10e7b5809b8309
escowin <edwin@escowinart.com>
2023-03-30 14:28:17 -0500

    modularized entry form to eliminate redundancies in

--- public/javascript/add-entry.js
+++ public/javascript/add-entry.js
@@ -1,37 +1,43 @@
 // logic
+function postEntry(journal_id, entry_date, entry_weight, entry_text) {
+  console.log(`
+  ${journal_id}
+  ${entry_date}
+  ${entry_weight}
+  ${entry_text}`
+  );
+}
+
+function postTagName(tag_name) {
+  console.log(tag_name);
+}
+
 async function entryFormHandler(e) {
   e.preventDefault();
 
   // entry data
-  const entry_date = document.getElementById("date").value.trim().replace(/-/g, ".");
+  const entry_date = document
+    .getElementById("date")
+    .value.trim()
+    .replace(/-/g, ".");
   const entry_weight = document.getElementById("weight").value.trim();
   const entry_text = document.getElementById("text").value.trim();
-  const journal_id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
-  ].split("?")[0]
+  const journal_id = window.location
+    .toString()
+    .split("/")
+    [window.location.toString().split("/").length - 1].split("?")[0];
 
   // tag data
   const tag_name = document.getElementById("tag-name").value.trim();
-  console.log(tag_name)
-  // if (entry_text) {
-  //   const response = await fetch("/api/entries", {
-  //     method: "post",
-  //     body: JSON.stringify({
-  //       journal_id,
-  //       entry_date,
-  //       entry_weight,
-  //       entry_text,
-  //     }),
-  //     headers: { "Content-Type": "application/json" },
-  //   });
 
-  //   if (response.ok) {
-  //     document.location.reload();
-  //     console.log("ok")
-  //   } else {
-  //     alert(response.statusText);
-  //   }
-  // }
+  if (entry_text && tag_name) {
+    postEntry(journal_id, entry_date, entry_weight, entry_text);
+    postTagName(tag_name);
+  } else if (entry_text) {
+    postEntry(journal_id, entry_date, entry_weight, entry_text);
+  } else {
+    return console.log("entry text required");
+  }
 }
 
 // calls
