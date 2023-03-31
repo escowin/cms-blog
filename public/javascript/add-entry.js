9cb28935b6cdee6158db9f7c11bdce03910b1f37
escowin <edwin@escowinart.com>
2023-03-31 13:53:06 -0500

    reconfigured posting an entry, conditionally logged tag name, added try/catch

--- public/javascript/add-entry.js
+++ public/javascript/add-entry.js
@@ -1,51 +1,82 @@
 // logic
 async function postEntry(journal_id, entry_date, entry_weight, entry_text) {
   // console.log("POST entry")
-  // try {
-  const response = await fetch("/api/entries", {
-    method: "post",
-    body: JSON.stringify({
-      journal_id,
-      entry_date,
-      entry_weight,
-      entry_text,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+  try {
+    const response = await fetch("/api/entries", {
+      method: "post",
+      body: JSON.stringify({
+        journal_id,
+        entry_date,
+        entry_weight,
+        entry_text,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-  if (response.ok) {
+    if (!response.ok) {
+      throw new Error("throw | failed to create entry.");
+    }
+
+    const newEntry = await response.json();
+    console.log(newEntry);
+
+    const tagResponse = await fetch("/api/tags", {
+      method: "post",
+      body: JSON.stringify({ tag_name }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (!tagResponse.ok) {
+      throw new Error("throw | `post` Tag failed");
+    }
+
+    const newTag = await tagResponse.json();
+    console.log(newTag);
+
+    // associates the entry with the tag(s) by posting json obj to the 'entryTags' through table model
+    const entryTagResponse = await fetch("/api/entrytags", {
+      method: "post",
+      body: JSON.stringify({
+        entry_id: newEntry.id,
+        tag_id: newTag.id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    console.log(entryTagResponse);
+    if (!entryTagResponse) {
+      throw new Error("throw | `post` entryTags failed");
+    }
+
+    console.log("successfully posted entry with tag");
     document.location.reload();
-    console.log("posted entry");
-  } else {
-    alert(response.statusText);
+  } catch (err) {
+    console.log(err);
+    alert("catch | posting entry with tag failed");
   }
-  // } catch (err) {
-  //   console.err(err)
-  //   alert('failed to post entry')
-  // }
 }
 
 // bug front end doesn't post tag. need to revisit routes & controllers
-async function postTagName(tag_name) {
-  console.log(tag_name);
-  // try {
-  const response = await fetch("/api/tags", {
-    method: "post",
-    body: JSON.stringify({ tag_name }),
-    headers: { "Content-Type": "application/json" },
-  });
+// async function postTagName(tag_name) {
+//   console.log(tag_name);
+//   // try {
+//   const response = await fetch("/api/tags", {
+//     method: "post",
+//     body: JSON.stringify({ tag_name }),
+//     headers: { "Content-Type": "application/json" },
+//   });
 
-  if (response.ok) {
-    document.location.reload();
-    console.log("posted tag");
-  } else {
-    alert(response.statusText);
-  }
-  // } catch (err) {
-  //   console.err(err)
-  //   alert('failed to post entry')
-  // }
-}
+//   if (response.ok) {
+//     document.location.reload();
+//     console.log("posted tag");
+//   } else {
+//     alert(response.statusText);
+//   }
+//   // } catch (err) {
+//   //   console.err(err)
+//   //   alert('failed to post entry')
+//   // }
+// }
 
 async function entryFormHandler(e) {
   e.preventDefault();
@@ -65,13 +96,38 @@ async function entryFormHandler(e) {
   // tag data
   const tag_name = document.getElementById("tag-name").value.trim();
 
-  if (entry_text && tag_name) {
-    await postEntry(journal_id, entry_date, entry_weight, entry_text);
-    await postTagName(tag_name);
-  } else if (entry_text) {
-    await postEntry(journal_id, entry_date, entry_weight, entry_text);
+  if (entry_text) {
+    try {
+      const response = await fetch("/api/entries", {
+        method: "POST",
+        body: JSON.stringify({
+          journal_id,
+          entry_date,
+          entry_weight,
+          entry_text,
+        }),
+      headers: { "Content-Type": "application/json" },
+      });
+      if (!response.ok) {
+        throw new Error("throw | posting entry failed")
+      }
+
+      // handles when an entry includes a tag
+      if (tag_name) {
+        console.log(tag_name)
+      }
+    } catch (err) {
+      console.log(err);
+      alert("catch | posting the entry or tag failed")
+    } 
+  //   await postEntry(journal_id, entry_date, entry_weight, entry_text);
+  //   // await postTagName(tag_name);
+  // } else if (entry_text) {
+  //   await postEntry(journal_id, entry_date, entry_weight, entry_text);
+  // } else {
+  //   return console.log("entry text required");
   } else {
-    return console.log("entry text required");
+    console.log("else | entry text (notes) required")
   }
 }
 
