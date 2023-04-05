abd27ba7905b35336b04348aad534a8a9337d801
escowin <edwin@escowinart.com>
2023-04-05 13:27:52 -0500

    configured resftul api entry routes

--- controllers/api/tag-routes.js
+++ controllers/api/tag-routes.js
@@ -35,10 +35,11 @@ router.get("/:id", ({ params }, res) => {
 });
 
 router.post("/", ({ body }, res) => {
-  console.log(body)
-  // Tag.create(body)
-  //   .then((tag) => res.status(200).json(tag))
-  //   .catch((err) => res.status(404).json(err));
+  Tag.create({
+    tag_name: body.tag_name
+  })
+    .then((tag) => res.status(200).json(tag))
+    .catch((err) => res.status(404).json(err));
 });
 
 
