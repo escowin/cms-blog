776a81e1eaf44698ff7473785cd6043530da7b19
escowin <edwin@escowinart.com>
2023-04-13 17:59:56 -0500

    configured an svg icon with onclick attribute to act as form button

--- public/javascript/add-journal.js
+++ public/javascript/add-journal.js
@@ -1,8 +1,9 @@
 // logic
 async function newJournalFormHandler(e) {
-  e.preventDefault();
+  // e.preventDefault();
+  console.log("add journal button clicked")
 
-  const title = document.querySelector('input[name="journal-title"]').value.trim();
+  const title = document.getElementById('journal-title').value.trim();
   const start_date = document.getElementById("start-date").value.trim();
   const end_date = document.getElementById("end-date").value.trim();
   const duration = document.getElementById("duration").value.trim();
@@ -10,21 +11,22 @@ async function newJournalFormHandler(e) {
     .querySelector('textarea[name="journal-description"]')
     .value.trim();
 
+    console.log(title)
   // user_id is obtained from the session in controllers/api/journal-routes
-  const response = await fetch(`/api/journals`, {
-    method: "post",
-    body: JSON.stringify({ title, start_date, end_date, duration, description }),
-    headers: { "Content-Type": "application/json" },
-  });
+  // const response = await fetch(`/api/journals`, {
+  //   method: "post",
+  //   body: JSON.stringify({ title, start_date, end_date, duration, description }),
+  //   headers: { "Content-Type": "application/json" },
+  // });
 
-  if (response.ok) {
-    document.location.replace("/dashboard");
-  } else {
-    alert(response.statusText);
-  }
+  // if (response.ok) {
+  //   document.location.replace("/dashboard");
+  // } else {
+  //   alert(response.statusText);
+  // }
 }
 
 // calls
 document
-  .getElementById("new-journal-form")
+  .getElementById("add-journal-btn")
   .addEventListener("submit", newJournalFormHandler);
