c926c19ecec3144a71e817f1d82509a0dd2ce122
escowin <edwin@escowinart.com>
2023-04-16 00:34:33 -0500

    set up delete fetch request with granular error handling

--- public/javascript/delete-journal.js
+++ public/javascript/delete-journal.js
@@ -1,13 +1,26 @@
-const deleteButtons = document.querySelectorAll('.delete-journal-btn');
+const deleteButtons = document.querySelectorAll(".delete-journal-btn");
 
 async function deleteJournalHandler(e) {
-    const button = e.target;
-    const journalEl = button.closest('.journal');
-    const journalId = journalEl.dataset.id;
-    console.log(journalEl)
-    console.log(journalId)
+  const button = e.target;
+  const journalEl = button.closest(".journal");
+  const journalId = journalEl.dataset.id;
+
+  try {
+    const response = await fetch(`/api/journals/${journalId}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      journalEl.remove();
+      document.location.reload();
+    } else {
+      throw new Error(`failed to delete journal #${journalId}`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-deleteButtons.forEach(button => {
-    button.addEventListener('click', deleteJournalHandler)
-})
+deleteButtons.forEach((button) => {
+  button.addEventListener("click", deleteJournalHandler);
+});
