6ae589004b38aa0b1df83ff7262237b7ad3fe5b5
escowin <edwin@escowinart.com>
2023-04-15 22:33:29 -0500

    reordered frontend journal js for legibility. logged all data points for POST request

--- public/javascript/add-journal.js
+++ public/javascript/add-journal.js
@@ -16,28 +16,26 @@ function durationRange() {
     selectedDuration = this.value;
     updateEndDate();
   });
-}
-
-function calculateEndDate(start_date, duration) {
-  const startDateObj = new Date(start_date);
-  console.log(startDateObj);
-  const durationInMs = parseInt(duration) * 7 * 24 * 60 * 60 * 1000;
-  const endDateObj = new Date(startDateObj.getTime() + durationInMs);
-  const endDate = endDateObj.toISOString().split("T")[0];
-  return endDate;
-  // const journalEndEl = document.getElementById("journal-end")
-  // journalEndEl.textContent = endDate;
-  // console.log(endDate)
-}
+};
 
 function updateEndDate() {
   const start_date = document.getElementById("journal-start").value.trim();
   const duration = selectedDuration;
-  const end_date = calculateEndDate(start_date, duration);
 
-  const journalEndEl = document.getElementById("journal-end");
-  journalEndEl.textContent = end_date;
-}
+  if (start_date && duration) {
+    const end_date = calculateEndDate(start_date, duration);
+    const journalEndEl = document.getElementById("journal-end");
+    journalEndEl.textContent = end_date;
+  }
+};
+
+function calculateEndDate(start_date, duration) {
+  const startDateObj = new Date(start_date);
+  const durationInMs = parseInt(duration) * 7 * 24 * 60 * 60 * 1000;
+  const endDateObj = new Date(startDateObj.getTime() + durationInMs);
+  const endDate = endDateObj.toISOString().split("T")[0];
+  return endDate;
+};
 
 // logic
 async function newJournalFormHandler() {
@@ -46,10 +44,14 @@ async function newJournalFormHandler() {
     .getElementById("journal-description")
     .value.trim();
   const start_date = document.getElementById("journal-start").value.trim();
-  // console.log(start_date)
   const duration = selectedDuration;
   const end_date = calculateEndDate(start_date, duration);
-  console.log(end_date);
+
+  console.log(title)
+  console.log(description)
+  console.log(start_date)
+  console.log(duration)
+  console.log(end_date)
 
   // user_id is obtained from the session in controllers/api/journal-routes
   // const response = await fetch(`/api/journals`, {
@@ -59,13 +61,12 @@ async function newJournalFormHandler() {
   // });
 
   // if (response.ok) {
-  //   document.location.replace("/dashboard");
+  //   document.location.replace("/");
   // } else {
   //   alert(response.statusText);
   // }
-}
+};
 
 // calls
 durationRange();
-updateEndDate();
 addJournalBtn.addEventListener("click", newJournalFormHandler);
