d8017b2031cfbac8a2718e8893bd206012f10d75
escowin <edwin@escowinart.com>
2023-04-16 12:54:40 -0500

    populated existing journal data into edit-journal form

---
+++ public/javascript/edit-journal.js
@@ -0,0 +1,10 @@
+// variables
+const updateJournalBtn = document.getElementById("add-journal-btn");
+
+// logic
+async function editJournalFormHandler(e) {
+  console.log("clicked");
+}
+
+// calls
+updateJournalBtn.addEventListener("click", editJournalFormHandler);
