709cbc9adbcb3a01ea3e5f860e1ca2cfb63f6576
escowin <edwin@escowinart.com>
2023-04-15 21:57:58 -0500

    displayed end date in browser when start & duration values are selected

--- public/javascript/add-journal.js
+++ public/javascript/add-journal.js
@@ -1,3 +1,4 @@
+const addJournalBtn = document.getElementById("add-journal-btn");
 let selectedDuration = null;
 
 function durationRange() {
@@ -13,28 +14,42 @@ function durationRange() {
   // captures selected duration value
   journalDurationEl.addEventListener("change", function () {
     selectedDuration = this.value;
+    updateEndDate();
   });
 }
 
 function calculateEndDate(start_date, duration) {
   const startDateObj = new Date(start_date);
+  console.log(startDateObj);
   const durationInMs = parseInt(duration) * 7 * 24 * 60 * 60 * 1000;
   const endDateObj = new Date(startDateObj.getTime() + durationInMs);
-
   const endDate = endDateObj.toISOString().split("T")[0];
-  console.log(endDate)
+  return endDate;
+  // const journalEndEl = document.getElementById("journal-end")
+  // journalEndEl.textContent = endDate;
+  // console.log(endDate)
+}
+
+function updateEndDate() {
+  const start_date = document.getElementById("journal-start").value.trim();
+  const duration = selectedDuration;
+  const end_date = calculateEndDate(start_date, duration);
+
+  const journalEndEl = document.getElementById("journal-end");
+  journalEndEl.textContent = end_date;
 }
 
 // logic
 async function newJournalFormHandler() {
-  // e.preventDefault();
   const title = document.getElementById("journal-title").value.trim();
   const description = document
-  .getElementById("journal-description")
-  .value.trim();
+    .getElementById("journal-description")
+    .value.trim();
   const start_date = document.getElementById("journal-start").value.trim();
+  // console.log(start_date)
   const duration = selectedDuration;
   const end_date = calculateEndDate(start_date, duration);
+  console.log(end_date);
 
   // user_id is obtained from the session in controllers/api/journal-routes
   // const response = await fetch(`/api/journals`, {
@@ -52,6 +67,5 @@ async function newJournalFormHandler() {
 
 // calls
 durationRange();
-document
-  .getElementById("add-journal-btn")
-  .addEventListener("click", newJournalFormHandler);
+updateEndDate();
+addJournalBtn.addEventListener("click", newJournalFormHandler);
