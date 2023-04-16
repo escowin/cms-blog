708672e53ed3e58a178e57d307ccfba091313f27
escowin <edwin@escowinart.com>
2023-04-16 00:32:02 -0500

    targeted journal by its data-id value

--- public/javascript/delete-journal.js
+++ public/javascript/delete-journal.js
@@ -1,7 +1,11 @@
 const deleteButtons = document.querySelectorAll('.delete-journal-btn');
 
 async function deleteJournalHandler(e) {
-    console.log("clicked")
+    const button = e.target;
+    const journalEl = button.closest('.journal');
+    const journalId = journalEl.dataset.id;
+    console.log(journalEl)
+    console.log(journalId)
 }
 
 deleteButtons.forEach(button => {
