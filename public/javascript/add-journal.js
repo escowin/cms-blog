51b2d9ec6275626b935163e71fbfab1f5ba29193
escowin <edwin@escowinart.com>
2023-04-15 23:08:31 -0500

    refactored journal form js

--- public/javascript/add-journal.js
+++ public/javascript/add-journal.js
@@ -1,26 +1,22 @@
 const addJournalBtn = document.getElementById("add-journal-btn");
-let selectedDuration = null;
+const journalTitleEl = document.getElementById("journal-title");
+const journalDescriptionEl = document.getElementById("journal-description");
+const journalDurationEl = document.getElementById("journal-duration");
+const journalStartEl = document.getElementById("journal-start");
+const journalEndEl = document.getElementById("journal-end");
 
 function durationRange() {
-  const journalDurationEl = document.getElementById("journal-duration");
-
   for (let i = 1; i <= 20; i++) {
     const option = document.createElement("option");
     option.value = i;
     option.text = i;
     journalDurationEl.appendChild(option);
   }
-
-  // captures selected duration value
-  journalDurationEl.addEventListener("change", function () {
-    selectedDuration = this.value;
-    updateEndDate();
-  });
 }
 
 function updateEndDate() {
-  const start_date = document.getElementById("journal-start").value.trim();
-  const duration = selectedDuration;
+  const start_date = journalStartEl.value.trim();
+  const duration = journalDurationEl.value.trim();
 
   if (start_date && duration) {
     const end_date = calculateEndDate(start_date, duration);
@@ -42,12 +38,10 @@ async function newJournalFormHandler(e) {
   // placed here for best practices. the svg icon acting as a button does not have default behavior.
   e.preventDefault();
 
-  const title = document.getElementById("journal-title").value.trim();
-  const description = document
-    .getElementById("journal-description")
-    .value.trim();
-  const duration = selectedDuration;
-  const start_date = document.getElementById("journal-start").value.trim();
+  const title = journalTitleEl.value.trim();
+  const description = journalDescriptionEl.value.trim();
+  const duration = journalDurationEl.value.trim();
+  const start_date = journalStartEl.value.trim();
   const end_date = calculateEndDate(start_date, duration);
 
   if (title && description && duration && start_date && end_date) {
@@ -80,6 +74,7 @@ async function newJournalFormHandler(e) {
   }
 }
 
-// calls
+// calls & event listeners
 durationRange();
+journalDurationEl.addEventListener("change", updateEndDate);
 addJournalBtn.addEventListener("click", newJournalFormHandler);
