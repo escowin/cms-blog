42efdf4bfd07308e1654d377ca1f1ba422106520
escowin <edwin@escowinart.com>
2023-04-30 00:50:37 -0500

    cleaned up repo references for consistency

--- public/javascript/copyright.js
+++ public/javascript/copyright.js
@@ -1,9 +1,9 @@
 function currentYear() {
     let year = new Date().getFullYear();
     console.log(`
-    \u00A9 ${year} Edwin M. Escobar
-    https://github.com/escowin/tech-blog
-    `);
+ \u00A9 ${year} Edwin M. Escobar
+ https://github.com/escowin/fitness-logbook
+ `);
 
     const yearEl = document.getElementById('year');
     yearEl.innerText = year;
