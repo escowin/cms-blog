c23909bcb0fcfbeff96909f7c4a6577ab64218eb
escowin <edwin@escowinart.com>
2023-09-19 15:28:03 -0500

    added postgres dependency & env variables for render deployment

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,17 @@ require("dotenv").config();
 
 let sequelize;
 // heroku & local deployment, latter relies on local .env
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.PORT) {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.PROD_HOST,
+      dialect: process.env.PROD_DIALECT,
+      port: process.env.PROD_PORT,
+    }
+  );
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
